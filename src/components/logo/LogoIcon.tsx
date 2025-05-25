4290547ddcfd9012e2590dd84ea360dcf1653bce
Ayub Mohamed <mayub4806@gmail.com>
2025-05-24 19:31:23 -0700

    Add book menu items and update logo components

--- src/components/logo/LogoIcon.tsx
+++ src/components/logo/LogoIcon.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 // ==============================|| LOGO ICON SVG ||============================== //
 
-const logoIcon = 'assets/images/logo-icon.svg';
+const logoIcon = '/assets/images/logo-icon.svg';
 
 export default function LogoIcon() {
   return <Image src={logoIcon} alt="UW" width={55} height={55} />;
