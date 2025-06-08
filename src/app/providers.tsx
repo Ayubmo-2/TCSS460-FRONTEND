dc9531e2b7fa2c805dde95578d5a69c8f83c3c54
Andrew Hwang <139718600+AndrewHwang46@users.noreply.github.com>
2025-06-08 02:51:48 -0700

    Revert "testing new change"
    This reverts commit b634daf425d5aa97eecb6bd44fd38d41fc58619e.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { IntlProvider } from 'react-intl';
+import { SessionProvider } from 'next-auth/react';
 import enMessages from '@/locales/en.json';
 import ThemeCustomization from '@/themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeCustomization>
-      <IntlProvider messages={enMessages} locale="en" defaultLocale="en">
-        {children}
-      </IntlProvider>
-    </ThemeCustomization>
+    <SessionProvider>
+      <ThemeCustomization>
+        <IntlProvider messages={enMessages} locale="en" defaultLocale="en">
+          {children}
+        </IntlProvider>
+      </ThemeCustomization>
+    </SessionProvider>
   );
-}
+} 
